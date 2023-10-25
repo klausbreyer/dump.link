5f968f179477bcc71e93abbc4adb5f0c5d20f874
Klaus Breyer <kb@v01.io>
2023-10-25 14:39:40 +0200

    Icons (#5)
    * icons

--- src/common/icons.tsx
+++ src/common/icons.tsx
@@ -36,3 +36,179 @@ export const EjectIcon: React.FC<SVGProps<SVGSVGElement>> = (props) => {
     </svg>
   );
 };
+export const GroupingIcon: React.FC<React.SVGProps<SVGSVGElement>> = (
+  props,
+) => {
+  return (
+    <svg
+      width="1229"
+      height="1229"
+      version="1.1"
+      viewBox="0 0 1229 1229"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <title>grouping</title>
+      <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+        <rect
+          fill="currentColor"
+          x="497"
+          y="382"
+          width="323"
+          height="116"
+        ></rect>
+        <rect
+          fill="currentColor"
+          x="497"
+          y="555"
+          width="323"
+          height="116"
+        ></rect>
+        <rect
+          fill="currentColor"
+          x="497"
+          y="729"
+          width="323"
+          height="116"
+        ></rect>
+        <rect
+          fill="currentColor"
+          x="497"
+          y="902"
+          width="323"
+          height="116"
+        ></rect>
+        <rect
+          fill="currentColor"
+          x="905"
+          y="207"
+          width="323"
+          height="116"
+        ></rect>
+        <rect
+          fill="currentColor"
+          x="905"
+          y="382"
+          width="323"
+          height="116"
+        ></rect>
+        <rect
+          fill="currentColor"
+          x="905"
+          y="556"
+          width="323"
+          height="115"
+        ></rect>
+        <rect
+          fill="currentColor"
+          x="905"
+          y="729"
+          width="323"
+          height="116"
+        ></rect>
+        <rect
+          fill="currentColor"
+          x="906"
+          y="902"
+          width="323"
+          height="116"
+        ></rect>
+        <rect
+          fill="currentColor"
+          x="497"
+          y="207"
+          width="323"
+          height="116"
+        ></rect>
+        <rect fill="currentColor" x="0" y="207" width="397" height="811"></rect>
+      </g>
+    </svg>
+  );
+};
+
+export const OrderingIcon: React.FC<React.SVGProps<SVGSVGElement>> = (
+  props,
+) => {
+  return (
+    <svg
+      width="1229"
+      height="1229"
+      version="1.1"
+      viewBox="0 0 1229 1229"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <title>ordering</title>
+      <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+        <path
+          d="M445.475976,355.648545 L463.413716,373.06226 L726.166,628.138 L797.214242,554.95293 L891,823 L620.290889,737.200365 L691.338,664.014 L428.586284,408.93774 L410.648545,391.524024 L445.475976,355.648545 Z"
+          fill="currentColor"
+          fillRule="nonzero"
+        ></path>
+        <path
+          d="M284.763769,411.593236 L332.406764,426.763769 L324.821497,450.585266 L280.887,588.558 L378.079512,619.50674 L180,823 L136.053099,542.440434 L233.244,573.388 L277.178503,435.414734 L284.763769,411.593236 Z"
+          fill="currentColor"
+          fillRule="nonzero"
+        ></path>
+        <rect
+          fill="currentColor"
+          x="121"
+          y="215"
+          width="360"
+          height="228"
+        ></rect>
+        <rect fill="currentColor" x="0" y="797" width="360" height="228"></rect>
+        <rect
+          fill="currentColor"
+          x="869"
+          y="797"
+          width="360"
+          height="228"
+        ></rect>
+      </g>
+    </svg>
+  );
+};
+
+export const SequencingIcon: React.FC<React.SVGProps<SVGSVGElement>> = (
+  props,
+) => {
+  return (
+    <svg
+      width="1229"
+      height="1229"
+      viewBox="0 0 1229 1229"
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+        <rect
+          fill="currentColor"
+          x="869"
+          y="579"
+          width="360"
+          height="228"
+        ></rect>
+        <rect
+          fill="currentColor"
+          x="455"
+          y="208"
+          width="360"
+          height="228"
+        ></rect>
+        <rect fill="currentColor" x="0" y="785" width="360" height="233"></rect>
+        <path
+          d="M547,412 L468.266295,684.84795 L393.258,615.726 L177.313195,850.061895 L160.371645,868.446233 L123.602968,834.563134 L140.544518,816.178796 L356.489,581.842 L281.481417,512.721806 L547,412 Z"
+          fill="currentColor"
+          fillRule="nonzero"
+        ></path>
+        <path
+          d="M942.808431,683.895322 L960.104678,730.808431 L936.648124,739.456555 L580.967,870.591 L616.251064,966.294361 L334,935 L528.386127,727.975766 L563.67,823.678 L919.351876,692.543445 L942.808431,683.895322 Z"
+          fill="currentColor"
+          fillRule="nonzero"
+        ></path>
+      </g>
+    </svg>
+  );
+};
