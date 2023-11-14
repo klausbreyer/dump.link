dcd45b377a6ee84588baed43602a00614746f25b
Klaus Breyer <kb@v01.io>
2023-11-14 11:13:51 +0100

    static build is working

--- api/src/handler.go
+++ api/src/handler.go
@@ -5,6 +5,7 @@ import (
 	"io/fs"
 	"log"
 	"net/http"
+	"path/filepath"
 	"strings"
 
 	"github.com/gomarkdown/markdown"
@@ -23,6 +24,10 @@ func (s *Server) handleError(w http.ResponseWriter, r *http.Request, err error) 
 	http.Error(w, fmt.Sprintf("Error: %s", id), http.StatusInternalServerError)
 }
 
+func (s *Server) HealthGet(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprint(w, "healthy")
+}
+
 func (s *Server) RootGet(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 
 	mdContent, err := fs.ReadFile(s.contentFS, "content/index.md")
@@ -33,7 +38,8 @@ func (s *Server) RootGet(w http.ResponseWriter, r *http.Request, _ httprouter.Pa
 	}
 
 	htmlContent := markdown.ToHTML(mdContent, nil, nil)
-	htmlTemplate := `
+
+	template := fmt.Sprintf(`
 <!DOCTYPE html>
 <html>
 <head>
@@ -43,17 +49,67 @@ func (s *Server) RootGet(w http.ResponseWriter, r *http.Request, _ httprouter.Pa
 <link rel="stylesheet" href="/static/tailwind.css" />
 </head>
 <body>
-{{.Content}}
+%s
 </body>
 </html>
-`
-
-	finalHTML := strings.Replace(htmlTemplate, "{{.Content}}", string(htmlContent), 1)
+`, string(htmlContent))
 
 	w.Header().Set("Content-Type", "text/html")
-	w.Write([]byte(finalHTML))
+	w.Write([]byte(template))
 }
 
-func (s *Server) HealthGet(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	fmt.Fprint(w, "healthy")
+// AppGet handles the request and serves the HTML with dynamic script and stylesheet links.
+func (s *Server) AppGet(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	jsFile, cssFile, err := findFiles("static/app/")
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Println("jsFile: ", jsFile)
+	fmt.Println("cssFile: ", cssFile)
+
+	template := fmt.Sprintf(`
+	<!DOCTYPE html>
+	<html>
+	<head>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link href="/%s" rel="stylesheet">
+	</head>
+	<body>
+		<div class="flex items-center justify-center w-screen h-screen text-center lg:hidden">
+			This site is not optimized for mobile. Please use a desktop browser.
+		</div>
+		<div id="app" class="hidden lg:block"></div>
+		<script src="/%s" type="module"></script>
+	</body>
+	</html>
+	`, cssFile, jsFile)
+
+	w.Header().Set("Content-Type", "text/html")
+	w.Write([]byte(template))
+}
+
+// findFiles scans the given directory for JS and CSS files and returns their paths.
+func findFiles(dir string) (jsFile, cssFile string, err error) {
+	err = filepath.WalkDir(dir, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if d.IsDir() {
+			return nil
+		}
+
+		if strings.HasSuffix(path, ".js") {
+			jsFile = path
+		} else if strings.HasSuffix(path, ".css") {
+			cssFile = path
+		}
+
+		return nil
+	})
+
+	return jsFile, cssFile, err
 }
