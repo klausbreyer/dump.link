035ef8b4cbce3ca033e916e7e951bef11941ee35
Klaus Breyer <kb@v01.io>
2023-11-17 11:05:35 +0100

    group: remove separation bar

--- app/src/Group.tsx
+++ app/src/Group.tsx
@@ -25,7 +25,7 @@ const Group: React.FC<GroupProps> = (props) => {
   return (
     <Container>
       <div className="grid grid-cols-3 gap-4 mb-10">
-        <div className="pr-4 border-r-2 border-slate-500">
+        <div className="">
           <Area bucket={dump} />
         </div>
         <div className="col-span-2">
