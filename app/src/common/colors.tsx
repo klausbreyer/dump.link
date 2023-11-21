65814ebd3d76a8e2e7f133f587a4d645b19e23ed
Klaus Breyer <kb@v01.io>
2023-11-21 13:45:05 +0100

    fix flagged behaviour

--- app/src/common/colors.tsx
+++ app/src/common/colors.tsx
@@ -34,14 +34,13 @@ export function getBucketBackgroundColorTop(
 }
 
 export function getFlagButtonBackground(bucket: Bucket, tasks: Task[]): string {
-  const slate = "bg-slate-400 hover:bg-slate-500 focus:bg-slate-500";
+  const slate = "bg-slate-200 hover:bg-slate-300 focus:bg-slate-300";
   const statusToColor = {
     [BucketState.UNSOLVED]: slate,
     [BucketState.SOLVED]: slate,
     [BucketState.DONE]: "bg-green-300 hover:bg-green-400 focus:bg-green-400",
     [BucketState.EMPTY]: slate,
-    [BucketState.INACTIVE]:
-      "bg-slate-200 hover:bg-slate-300 focus:bg-slate-300",
+    [BucketState.INACTIVE]: slate,
   };
 
   return statusToColor[getBucketState(bucket, tasks)];
@@ -49,7 +48,7 @@ export function getFlagButtonBackground(bucket: Bucket, tasks: Task[]): string {
 
 export function getBucketFlaggedStyle(bucket: Bucket, tasks: Task[]): string {
   const open = getTasksByClosed(tasks, false);
-  if (bucket.flagged && open.length > 0) {
+  if ((bucket.flagged && open.length > 0) || tasks.length === 0) {
     return " bg-rose-500 ";
   }
   return "";
