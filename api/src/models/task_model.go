2952b0c9f9123bb49ecbb6af084472b345188df2
Klaus Breyer <kb@v01.io>
2023-11-22 13:48:43 +0100

    fix: timestamp fields when creating task

--- api/src/models/task_model.go
+++ api/src/models/task_model.go
@@ -43,17 +43,28 @@ func (m *TaskModel) IDExists(id string) bool {
 }
 
 func (m *TaskModel) Get(id string) (*Task, error) {
-	stmt := `SELECT id, title, closed, bucket_id, priority FROM tasks WHERE id = ?`
+	stmt := `SELECT id, title, closed, bucket_id, priority, created_at, updated_at FROM tasks WHERE id = ?`
 	row := m.DB.QueryRow(stmt, id)
 
 	t := &Task{}
+	var createdAtStr, updatedAtStr string
 
-	err := row.Scan(&t.ID, &t.Title, &t.Closed, &t.BucketID, &t.Priority)
+	err := row.Scan(&t.ID, &t.Title, &t.Closed, &t.BucketID, &t.Priority, &createdAtStr, &updatedAtStr)
 	if err != nil {
 		if err == sql.ErrNoRows {
 			return nil, fmt.Errorf("Task with ID %s not found", id)
 		}
 		return nil, err
+	}
+
+	t.CreatedAt, err = time.Parse(DateTimeLayout, createdAtStr)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse createdAt: %v", err)
+	}
+
+	t.UpdatedAt, err = time.Parse(DateTimeLayout, updatedAtStr)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse updatedAt: %v", err)
 	}
 
 	return t, nil
