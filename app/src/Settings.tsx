5571cc81849e2d6146c29773c9bbaceee4897258
Klaus Breyer <kb@v01.io>
2023-11-27 15:42:52 +0100

    appetite + date field smaller. limit characters for process

--- app/src/Settings.tsx
+++ app/src/Settings.tsx
@@ -61,13 +61,14 @@ const Settings: React.FC<SettingsProps> = (props) => {
           <input
             type="text"
             id="name"
+            maxLength={40}
             value={name}
             onChange={handleChange(setName)}
             className="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
           />
         </div>
 
-        <div className="mb-4">
+        <div className="w-40 mb-4">
           <label
             htmlFor="startedAt"
             className="block text-sm font-medium text-gray-700"
@@ -83,7 +84,7 @@ const Settings: React.FC<SettingsProps> = (props) => {
           />
         </div>
 
-        <div className="mb-4">
+        <div className="w-40 mb-4">
           <label
             htmlFor="appetite"
             className="block text-sm font-medium text-gray-700"
