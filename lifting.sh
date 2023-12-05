a7707fe570f823f3c009391cf404cdf98ca98282
Klaus Breyer <kb@v01.io>
2023-12-05 11:43:19 +0100

    copying sourcemaps

--- lifting.sh
+++ lifting.sh
@@ -10,14 +10,18 @@ destination_dir="api/static/app"
 rm -rf $destination_dir/*
 echo "Cleared the contents of the destination directory."
 
-# Find and copy the CSS file
+# Find and copy the CSS file along with its source map
 css_file=$(find $source_dir -name '*.css')
+css_map_file=$(find $source_dir -name '*.css.map')
 cp $css_file $destination_dir/
-echo "Copied CSS file."
+cp $css_map_file $destination_dir/
+echo "Copied CSS file and its source map."
 
-# Find and copy the JS file
+# Find and copy the JS file along with its source map
 js_file=$(find $source_dir -name '*.js')
+js_map_file=$(find $source_dir -name '*.js.map')
 cp $js_file $destination_dir/
-echo "Copied JS file."
+cp $js_map_file $destination_dir/
+echo "Copied JS file and its source map."
 
 echo "Content of the target directory has been deleted and new files have been copied."
