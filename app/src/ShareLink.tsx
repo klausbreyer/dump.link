ab8c3d5695ee4e98c7cca2a25d588c4a69eb9dd8
Klaus Breyer <kb@v01.io>
2023-12-18 11:39:33 +0100

    Feature: Added tab selection and iFrame embed functionality to Share dialog

--- app/src/ShareLink.tsx
+++ app/src/ShareLink.tsx
@@ -2,22 +2,44 @@ import React, { useState, useRef } from "react";
 import { Tooltip } from "./common/InfoTooltip";
 import { currentUrl } from "./HeaderProject";
 
+type TabKey = "group" | "sequence" | "arrange";
+
 const ShareLink: React.FC = () => {
   const [isCopied, setIsCopied] = useState(false);
+  const [selectedTabKey, setSelectedTabKey] = useState<TabKey>("group"); // Default to 'group'
   const urlInputRef = useRef<HTMLInputElement>(null);
+  const iframeInputRef = useRef<HTMLInputElement>(null);
 
-  const copyToClipboard = async () => {
-    try {
-      await navigator.clipboard.writeText(currentUrl());
-      setIsCopied(true);
-      setTimeout(() => setIsCopied(false), 2000);
-    } catch (err) {
-      console.error("Failed to copy: ", err);
+  const tabs: Record<string, TabKey> = {
+    "Dump & Task Grouper": "group",
+    "Task Group Sequencer": "sequence",
+    "Task Group Arranger": "arrange",
+  };
+
+  const getTabUrl = (tabKey: string): string => {
+    return `${currentUrl()}?p=${encodeURIComponent(tabKey)}`;
+  };
+
+  const getEmbedCode = (tabKey: TabKey): string => {
+    return `<iframe src="${getTabUrl(
+      tabKey,
+    )}" style="min-width: 1024px; width: 100%; min-height: 800px;" frameborder="0"></iframe>`;
+  };
+
+  const copyToClipboard = async (ref: React.RefObject<HTMLInputElement>) => {
+    if (ref.current) {
+      try {
+        await navigator.clipboard.writeText(ref.current.value);
+        setIsCopied(true);
+        setTimeout(() => setIsCopied(false), 2000);
+      } catch (err) {
+        console.error("Failed to copy: ", err);
+      }
     }
   };
 
-  const selectText = () => {
-    urlInputRef.current?.select();
+  const selectText = (ref: React.RefObject<HTMLInputElement>) => {
+    ref.current?.select();
   };
 
   return (
@@ -31,24 +53,66 @@ const ShareLink: React.FC = () => {
         user logins, this link is the only way to access your board. Bookmarking
         it or keeping it in a safe place is highly recommended.
       </p>
-      <div className="flex items-baseline justify-between mb-2">
+      <div className="mb-2">
+        <label htmlFor="tab-select">Choose what tab to share / embed:</label>
+        <select
+          id="tab-select"
+          className="p-2 ml-2 border rounded-md cursor-pointer"
+          onChange={(e) => setSelectedTabKey(tabs[e.target.value])}
+          value={Object.keys(tabs).find((key) => tabs[key] === selectedTabKey)}
+        >
+          {Object.keys(tabs).map((tabName) => (
+            <option key={tabName} value={tabName}>
+              {tabName}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className="mb-2 ">
+        <label htmlFor="url-input">URL:</label>
         <Tooltip info="Click to copy">
           <input
             ref={urlInputRef}
             readOnly
-            className="p-2 mt-2 border rounded-md cursor-pointer w-80 border-slate-300"
-            value={currentUrl()}
+            className="w-full p-2 mt-2 border rounded-md cursor-pointer border-slate-300"
+            value={getTabUrl(selectedTabKey)}
             onClick={() => {
-              copyToClipboard();
-              selectText();
+              copyToClipboard(urlInputRef);
+              selectText(urlInputRef);
             }}
-            onFocus={selectText}
+            onFocus={() => selectText(urlInputRef)}
           />
         </Tooltip>
-        <p className={`font-bold ${isCopied ? "opacity-100" : "opacity-0"}`}>
-          Copied!
-        </p>
       </div>
+      <div className="mb-2">
+        <label htmlFor="iframe-input">iFrame Embed Code:</label>
+        <Tooltip info="Click to copy">
+          <input
+            ref={iframeInputRef}
+            readOnly
+            className="w-full p-2 mt-2 border rounded-md cursor-pointer border-slate-300"
+            value={getEmbedCode(selectedTabKey)}
+            onClick={() => {
+              copyToClipboard(iframeInputRef);
+              selectText(iframeInputRef);
+            }}
+            onFocus={() => selectText(iframeInputRef)}
+          />
+        </Tooltip>
+      </div>
+
+      <p>
+        Embedding via iFrame is great for platforms like Notion, Confluence, and
+        other tools that support iFrame. It allows you to integrate interactive
+        content directly into your documentation or collaboration space.
+      </p>
+      <p
+        className={`font-bold text-right ${
+          isCopied ? "opacity-100" : "opacity-0"
+        }`}
+      >
+        Copied!
+      </p>
     </>
   );
 };
